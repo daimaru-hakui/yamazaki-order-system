cf18c9264786dc2afe191e218defbf058953bd72
sukebon <39957486+sukebon@users.noreply.github.com>
2024-01-28 01:18:47 +0900

    csv table

--- src/components/order-csv/form-order-csv.tsx
+++ src/components/order-csv/form-order-csv.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Button, Input } from "@nextui-org/react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import OrderCsvList from "./order-csv-list";
 import { SkuWithProduct } from "@/app/order-csv/page";
 
@@ -9,44 +9,54 @@ interface FormOrderCsv {
 }
 
 export default function FormOrderCsv({ skus }: FormOrderCsv) {
-  const [fileUpload, setFileUpload] = useState<File[] | null>(null);
+  const [file, setFile] = useState<File | null>(null);
   const [data, setData] = useState<any>([]);
-  console.log(skus);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (!files) return;
-    setFileUpload((prev => [...Array.from(prev || []), ...Array.from(files)]));
+    const { files } = e.target;
+    if (files) {
+      setFile(files[0]);
+    }
   };
 
   const handleFileClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    if (!fileUpload) return;
-    const file = fileUpload[0];
+    if (!file) return;
     const reader = new FileReader();
-    reader.readAsText(file, "Shift_JIS");
+    reader?.readAsText(file, "Shift_JIS");
     reader.addEventListener("load", async function () {
       if (!reader.result) return;
       const csvFile = reader.result.toString();
       const csvSplit = csvFile.split(/\r?\n/);
       const csvArray = csvSplit.map((csv) => csv.split(','));
-      const header = csvArray.shift();
+      csvArray.shift();
 
       const csv = csvArray.map((item) => {
         return {
           productId: item[0],
-          sizeId: item[1]
+          sizeId: item[1],
+          quantity: item[2]
         };
       });
-      setData(csv);
+      const csvSkus = csv.map((item) => {
+        const sku = findSkus(Number(item.productId), Number(item.sizeId));
+        return { ...sku, quantity: item.quantity };
+      }).filter((sku) => sku.id);
+      setData(csvSkus);
     });
   };
 
-  console.log(data);
+  const findSkus = (productId: number, sizeId: number) => {
+    const sku = skus.find((sku) =>
+      sku.productId === productId && sku.sizeId === sizeId
+    );
+    return sku;
+  };
+
 
   return (
     <form className="flex flex-col ">
-      <Input type="file" accept='.csv' onChange={handleFileChange} />
+      <input type="file" accept='*.csv' onChange={handleFileChange} />
       <Button
         color="primary"
         className="mt-3"
--- src/components/order-csv/order-csv-list.tsx
+++ src/components/order-csv/order-csv-list.tsx
@@ -1,12 +1,46 @@
+'use client';
+import { SkuWithProduct } from "@/app/order-csv/page";
+import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+
 interface OrderCsvListProps {
-  data: any;
+  data: (SkuWithProduct & { quantity: number; })[];
 }
 export default function OrderCsvList({ data }: OrderCsvListProps) {
   return (
     <div>
-      {data.map((item: any, idx: number) => (
-        <div key={idx}>{item.productId}</div>
-      ))}
+      <Table className="mt-3" aria-label="order cart table">
+        <TableHeader>
+          <TableColumn className="text-center">品番</TableColumn>
+          <TableColumn className="text-center">品名</TableColumn>
+          <TableColumn className="text-center">サイズ</TableColumn>
+          <TableColumn className="text-center">価格</TableColumn>
+          <TableColumn className="text-center">数量</TableColumn>
+          <TableColumn className="text-center">合計</TableColumn>
+          {/* <TableColumn className="text-center">削除</TableColumn> */}
+        </TableHeader>
+        <TableBody>
+          {data.map((sku) => (
+            <TableRow key={sku.id}>
+              <TableCell>{sku.product.productNumber}</TableCell>
+              <TableCell>{sku.product.productName}</TableCell>
+              <TableCell className="text-center">{sku.size.name}</TableCell>
+              <TableCell className="text-right">
+                {sku.price.toLocaleString()}
+              </TableCell>
+              <TableCell className="text-right">
+                d
+              </TableCell>
+              <TableCell className="text-right">
+                {(sku.quantity * sku.price).toLocaleString()}円
+              </TableCell>
+              {/* <TableCell className="text-center">
+                <AiFillDelete className="mx-auto text-lg cursor-pointer"
+                  onClick={() => handleClickDelete(item.skuId)} />
+              </TableCell> */}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
